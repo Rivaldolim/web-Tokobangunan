f73ff5a5b725bf857106566d00fd8f84c0222270
Rivaldolim <126888076+Rivaldolim@users.noreply.github.com>
2024-04-18 17:27:59 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INDOGAME</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<header class="bg-warning text-white py-4">
+    <div class="container text-center">
+        <h1>Website Alat Bangunan</h1>
+    </div>
+</header>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-warning">
+    <div class="container">
+        <!-- Tombol hamburger untuk tampilan mobile -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Kontainer untuk link navigasi -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#Home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#katalog">Katalog Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#About">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#kirim">Contact</a>
+                </li>
+            </ul>
+            <a class="btn btn-outline-light" href="login.php">Login</a>
+        </div>
+    </div>
+</nav>
+
+
+
+<!-- Bagian Konten Home -->
+<section id= "Home">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://pintukamarmandi.com/wp-content/uploads/2017/06/header-artikel-pkm.jpg" 
+                class="d-block w-100" alt="..." style="max-height: 500px;">
+            </div>
+            <div class="carousel-item">
+                <img src="https://www.englishcafe.co.id/wp-content/uploads/2018/07/Kosakata-bahasa-Inggris-tentang-bahan-bangunan-dan-alat-perkakas-pertukangann.jpg"
+                class="d-block w-100" alt="..." style="max-height: 500px;">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+            <div class="carousel-item">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJEhkZxas-PrhXkOSilsrRhOl06f2-GZoAyg&s"
+                class="d-block w-100" alt="..." style="max-height: 500px;">
+                <div class="carousel-caption d-none d-md-block">
+                </div>
+            </div>
+        </div>
+    </div>  
+</section>
+
+
+<section id="katalog" class="py-5">
+    <div class="container">
+        <h2 class="text-center mb-4">Katalog Produk</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img src="https://images.tokopedia.net/img/cache/700/VqbcmM/2022/6/10/28de9411-67d3-40da-807c-352c4b8b5274.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">Set Alat Perkakas</h3>
+                        <p class="card-text">
+                        Alat perkakas adalah sekutu tak tergantikan dalam setiap proyek, dan kami di sini untuk memastikan Anda memiliki akses ke yang terbaik. Dengan koleksi kami yang luas dan beragam, Anda akan menemukan segala yang Anda butuhkan, dari palu hingga bor listrik, dari obeng hingga gergaji, semua dengan kualitas unggul dan desain ergonomis. Tak peduli apakah Anda seorang profesional atau penggemar DIY, alat-alat kami siap membantu Anda menyelesaikan setiap pekerjaan dengan presisi dan efisiensi. Dibuat dari bahan-bahan terbaik dan dengan teknologi terkini, alat perkakas kami didesain untuk bertahan lama dan memberikan hasil yang memuaskan. Mulailah menjelajahi koleksi kami sekarang dan temukan alat yang tepat untuk proyek Anda!
+                        <br>
+                        <span style="font-style: italic;">Login terlebih dahulu untuk membeli alat ini.</span>
+                        </p>
+
+                        <a href="login.php" class="btn btn-warning">Login</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img src="https://down-id.img.susercontent.com/file/id-11134207-7r98u-lpvvlb8qzylqb6" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">Gerinda</h3>
+                        <p class="card-text">
+                        Gerinda adalah alat yang tak tergantikan dalam berbagai proyek pemotongan, penghalusan, dan pemodalan. Dengan kekuatan dan kecepatan yang luar biasa, gerinda kami akan menjadi sekutu setia Anda dalam menyelesaikan setiap tugas. Didesain untuk kenyamanan dan kinerja maksimal, gerinda kami dilengkapi dengan fitur-fitur canggih untuk memastikan presisi dan keamanan selama penggunaan. Dari penghalusan permukaan hingga pemotongan bahan berat, gerinda kami dapat mengatasi segala tantangan dengan mudah. Dengan kualitas terbaik dan daya tahan yang tak tertandingi, Anda dapat mempercayakan gerinda kami untuk memberikan hasil yang memuaskan setiap saat. Jelajahi koleksi kami sekarang dan temukan gerinda yang tepat untuk meningkatkan produktivitas dan kualitas kerja Anda!
+                        <br>
+                        <span style="font-style: italic;">Login terlebih dahulu untuk membeli gerinda ini.</span>
+                    </p>
+                    <a href="login.php" class="btn btn-warning">Login</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//catalog-image/103/MTA-141458970/no-brand_no-brand_full01.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h3 class="card-title">Gergaji Mesin</h3>
+                        <p class="card-text">
+                        Gerakkan proyek-proyek Anda ke level berikutnya dengan gergaji mesin kami yang handal. Dirancang untuk memberikan pemotongan yang presisi dan efisien, gergaji mesin kami adalah alat yang tak tergantikan dalam berbagai aplikasi konstruksi dan kerajinan. Dengan kekuatan dan keandalan yang luar biasa, gergaji mesin kami memastikan Anda mendapatkan hasil yang sempurna setiap kali. Didesain dengan fitur-fitur canggih untuk meningkatkan produktivitas dan kenyamanan pengguna, gergaji mesin kami cocok untuk berbagai jenis bahan dan proyek. Mulailah menjelajahi koleksi kami sekarang dan temukan gergaji mesin yang tepat untuk meningkatkan efisiensi dan kualitas kerja Anda.
+                        <br>
+                        <span style="font-style: italic;">Login terlebih dahulu untuk membeli gergaji mesin ini.</span>
+                    </p>
+                    <a href="login.php" class="btn btn-warning">Login</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="About" class="py-5">
+    <div class="container">
+        <h2 class="text-center mb-4">About Us</h2>
+        <div class="row">
+            <div class="col-lg-6 text-center mx-auto">
+                <p>Website Alat Bangunan adalah platform daring yang menyediakan berbagai informasi terkait alat-alat dan material bangunan. Kami berkomitmen untuk memberikan sumber daya terpercaya dan berkualitas bagi para profesional dan penggemar DIY di industri konstruksi. Dengan liputan yang komprehensif tentang perkembangan teknologi dan tren terbaru dalam industri bangunan, kami bertujuan untuk menjadi mitra yang andal dalam setiap proyek pembangunan Anda.</p>
+                <p>Visi kami adalah menjadi sumber informasi terkemuka yang menginspirasi dan memfasilitasi inovasi dalam industri konstruksi. Kami berusaha untuk memenuhi kebutuhan para pengguna kami dengan menyediakan konten yang relevan, tutorial, dan ulasan produk yang informatif.</p>
+                <p>Misi kami adalah menyediakan platform yang mudah digunakan dan informatif bagi para profesional konstruksi, pelaku industri, dan penggemar DIY untuk meningkatkan pengetahuan, keterampilan, dan efisiensi dalam setiap proyek bangunan.</p>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-6 mx-auto">
+                <img src="https://png.pngtree.com/thumb_back/fw800/background/20230715/pngtree-cartoon-style-3d-illustration-of-engineers-technicians-and-industrial-workers-at-image_3877025.jpg" class="img-fluid" alt="Foto Tim">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<section id="kirim" class="bg-warning">
+    <div class="container mt-5">
+        <h2 class="text-center mb-4 text-white">Contact Us</h2>
+        <div class="row justify-content-center">
+            <div class="col-md-6 text-white">
+                <form>
+                    <div class="mb-3">
+                        <label for="nama" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="nama" placeholder="Masukkan nama anda">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" placeholder="Masukkan email Anda">
+                    </div>
+                    <div class="mb-3">
+                        <label for="pesan" class="form-label">Pesan</label>
+                        <textarea class="form-control" id="pesan" rows="3" placeholder="Masukkan pesan anda"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-dark">Kirim</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<footer class="bg-warning text-white py-3">
+    <div class="container text-center">
+        <p>&copy; 2024 Website Alat Bangunan. All rights reserved.</p>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
